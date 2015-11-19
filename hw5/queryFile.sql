3cf09ac779db997039594fc64ae152f9a3d5d9f9
Calvin Lui <clui951@berkeley.edu>
2015-11-19 15:36:15 -0800

    finished part 1

--- hw5/queryFile.sql
+++ hw5/queryFile.sql
@@ -5,8 +5,8 @@
 set transaction read write;
 
 
-/*Code goes here*/
-
+select s.name, s.age from s;
+select * from data;
 
 commit;
 
@@ -14,7 +14,10 @@ commit;
 set transaction read write;
 
 
-/*Code goes here*/
+insert into data (data.f1, data.f2) values (6, 60);
+insert into data (data.f1, data.f2) values (10, 33);
+insert into s (s.sid, s.age, s.name) values (6, 6, 'Michael');
+insert into s (s.sid, s.age, s.name) values (7, 60, 'Michelle');
 
 
 commit;
@@ -28,9 +31,13 @@ select * from s;
 /*Put everything in 1 transaction, as seen in above part 1*/
 
 
+set transaction read write;
 
-/*Code goes here*/
+update data set data.f2=1 where data.f1=1;
+update s set s.age=100 where s.name='Michelle';
+update data set data.f2=100 where data.f2=50;
 
+commit;
 
 
 /*========please do not remove=========*/
@@ -41,11 +48,12 @@ select * from s;
 /*Part 3: delete entries*/
 /*Put everything in 1 transaction, as seen in above part 1*/
 
+set transaction read write;
 
+delete from data where data.f1=2;
+delete from s;
 
-/*Code goes here*/
-
-
+commit;
 
 /*========please do not remove=========*/
 select * from data;
