515177064a424c588f1a84c966d24f3b6ae1b7ff
Calvin Lui <clui951@berkeley.edu>
2015-11-21 18:19:06 -0800

    finished task 2

--- hw5/queryTest.sql
+++ hw5/queryTest.sql
@@ -2,6 +2,14 @@
 
 set transaction read write;
 
+insert into data (data.f1, data.f2) values (6, 60);
+insert into data (data.f1, data.f2) values (10, 33);
+insert into s (s.sid, s.age, s.name) values (6, 6, 'Michael');
+insert into s (s.sid, s.age, s.name) values (7, 60, 'Michelle');
+update data set data.f2=1 where data.f1=1;
+
+### flush;
+rollback; 
 
 
 /* update table and end transaction above */
